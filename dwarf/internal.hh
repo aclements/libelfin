1de764079619a16e78845dca02a51dd854566b4f
Austin Clements <amdragon@mit.edu>
2013-09-28 13:06:19 -0400

    Move to_hex from dwarf/internal.hh to elf/to_hex.hh
    In preparation for pretty-printing ELF enums

--- dwarf/internal.hh
+++ dwarf/internal.hh
@@ -2,6 +2,7 @@
 #define _DWARFPP_INTERNAL_HH_
 
 #include "dwarf++.hh"
+#include "../elf/to_hex.hh"
 
 #include <stdexcept>
 #include <type_traits>
@@ -9,29 +10,6 @@
 #include <vector>
 
 DWARFPP_BEGIN_NAMESPACE
-
-template<typename T>
-std::string
-to_hex(T v)
-{
-        static_assert(std::is_integral<T>::value,
-                "to_hex applied to non-integral type");
-        if (v == 0)
-                return std::string("0");
-        char buf[sizeof(T)*2 + 1];
-        char *pos = &buf[sizeof(buf)-1];
-        *pos-- = '\0';
-        while (v && pos >= buf) {
-                int digit = v & 0xf;
-                if (digit < 10)
-                        *pos = '0' + digit;
-                else
-                        *pos = 'a' + (digit - 10);
-                pos--;
-                v >>= 4;
-        }
-        return std::string(pos + 1);
-}
 
 enum class format
 {
